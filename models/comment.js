5993944b5a9b23e3db1713d85c28e0ac9daa690c
Juan Abarca <abarcaj277@gmail.com>
2021-03-22 17:33:37 -0700

    created Comment model

---
+++ models/comment.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const commentSchema = mongoose.Schema(
+  {
+    comment: {
+      type: String,
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+commentSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.updatedAt;
+  },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+module.exports = Comment;
