e8fc7119a38c7f22fdf46ed68448df996c958da7
Juan Abarca <abarcaj277@gmail.com>
2021-03-22 20:09:06 -0700

    added a validator for register route

---
+++ utils/validators.js
@@ -0,0 +1,52 @@
+// middleware to validate that all needed fields are given
+// and that the fields meet the needed conditions
+
+const hasSpaces = (str) => {
+  return /\s/g.test(str);
+};
+
+const hasSpecialCharacter = (str) => {
+  return /[^a-z0-9_]/.test(str);
+};
+
+const validEmailFormat = (email) => {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+    email
+  );
+};
+
+const validateRegister = (req, res, next) => {
+  const { name, username, email, password } = req.body;
+  let errorMessages = [];
+
+  if (!name || name.length < 2) {
+    errorMessages.push("Full Name must be at least 2 characters long");
+  }
+
+  if (!username || username.length < 3 || username.length > 20) {
+    errorMessages.push("Username must be between 3 and 20 characters long");
+  }
+  if (hasSpecialCharacter(username)) {
+    errorMessages.push(
+      "Username can't contain capital letters, spaces, or special characters"
+    );
+  }
+
+  if (!password || password.length < 6 || hasSpaces(password)) {
+    errorMessages.push(
+      "Password must be at least 6 characters long and not have spaces"
+    );
+  }
+
+  if (!email || !validEmailFormat(email)) {
+    errorMessages.push("Invalid email format");
+  }
+
+  if (errorMessages.length) {
+    return res.status(400).json({ error: errorMessages });
+  }
+
+  next();
+};
+
+module.exports = { validateRegister };
