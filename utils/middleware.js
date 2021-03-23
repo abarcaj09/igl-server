9d285156366340c95e111aae5b7afe8190b563f8
Juan Abarca <abarcaj277@gmail.com>
2021-03-22 20:24:43 -0700

    added middleware to track requests and basic error handling

---
+++ utils/middleware.js
@@ -0,0 +1,31 @@
+// express middleware
+
+const log = require("./log");
+
+const requestLog = (req, res, next) => {
+  log.info(`Method: ${req.method}`);
+  log.info(`Path: ${req.path}`);
+  log.info(`Body: ${req.body}`);
+  log.info("----");
+  next();
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).json({ error: "Unknown Endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+  log.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).json({ error: "Malformatted ID" });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  requestLog,
+  unknownEndpoint,
+  errorHandler,
+};
