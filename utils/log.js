cf5e274f0cfce720bf2cf44bdb06ebd43c55c5df
Juan Abarca <abarcaj277@gmail.com>
2021-03-22 20:13:50 -0700

    added logging

---
+++ utils/log.js
@@ -0,0 +1,15 @@
+// log requests, response, and errors
+
+const info = (...params) => {
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
+  }
+};
+
+const error = (...params) => {
+  if (process.env.NODE_ENV !== "test") {
+    console.error(...params);
+  }
+};
+
+module.exports = { info, error };
