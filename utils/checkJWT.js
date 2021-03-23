a8a93d0bfde838458850523ce1e387bbdd6e48ec
Juan Abarca <abarcaj277@gmail.com>
2021-03-23 16:55:05 -0700

    added function to check that auth header is provided and is valid

---
+++ utils/checkJWT.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const checkJWT = (req, res, next) => {
+  const authHeader = req.get("authorization");
+  if (authHeader && authHeader.toLowerCase().startsWith("bearer ")) {
+    const token = authHeader.substring(7);
+    const verifiedToken = jwt.verify(token, process.env.SECRET);
+    req.userId = verifiedToken.id;
+    req.username = verifiedToken.username;
+    next();
+  } else {
+    res
+      .status(401)
+      .json({ error: "Authorization header is missing or invalid" });
+  }
+};
+
+module.exports = checkJWT;
