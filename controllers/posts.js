2471426143a62df9dc004fbb0a970f5871a99368
Juan Abarca <abarcaj277@gmail.com>
2021-03-24 23:55:52 -0700

    like and save pass all tests

--- controllers/posts.js
+++ controllers/posts.js
@@ -48,4 +48,71 @@ postsRouter.post("/", [checkJWT, validatePost], async (req, res) => {
   res.status(201).json({ post: savedPost });
 });
 
+// post will be liked if it isn't already like by the user, else the
+// like will be removed
+postsRouter.post("/:id/likes", checkJWT, async (req, res) => {
+  const post = await Post.findById(req.params.id).select("likes");
+  const user = await User.findById(req.userId).select("likes");
+
+  if (!post || !user) {
+    return res.status(400).json({ error: "User or post does not exist." });
+  }
+
+  const postLikeIndex = post.likes.indexOf(user.id);
+  const userLikeIndex = user.likes.indexOf(post.id);
+
+  if (postLikeIndex === -1) {
+    // user has NOT liked this post => like the post
+    post.likes.push(user.id);
+    await post.save();
+
+    user.likes.push(post.id);
+    await user.save();
+
+    return res.status(201).json({
+      userLikes: user.likes,
+    });
+  }
+
+  // user has liked this post => unlike the post
+  post.likes.splice(postLikeIndex, 1);
+  await post.save();
+
+  user.likes.splice(userLikeIndex, 1);
+  await user.save();
+
+  res.json({
+    userLikes: user.likes,
+  });
+});
+
+// post will be saved if it isn't already saved by the user, else the
+// save will be removed
+postsRouter.post("/:id/save", checkJWT, async (req, res) => {
+  const user = await User.findById(req.userId).select("saved");
+  const post = await Post.findById(req.params.id);
+
+  if (!post || !user) {
+    return res.status(400).json({ error: "User or post does not exist." });
+  }
+
+  const savedIndex = user.saved.indexOf(post.id);
+  if (savedIndex === -1) {
+    // user has NOT saved this post => save it
+    user.saved.push(post.id);
+    await user.save();
+
+    return res.status(201).json({
+      userSaved: user.saved,
+    });
+  }
+
+  // user has saved this post => remove it from saved
+  user.saved.splice(savedIndex, 1);
+  await user.save();
+
+  res.json({
+    userSaved: user.saved,
+  });
+});
 module.exports = postsRouter;
